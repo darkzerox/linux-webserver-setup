f59dc8f0e03021b34424592f5b9f23c9723459ae
Darkness Zerox <Darkness Zerox>
2016-07-31 01:48:46 +0700

    edit compass

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
 sudo apt-get install ruby-full --y
 sudo apt-get install make --y
-sudo apt-get install compass --y
+sudo gem install compass --y
 sudo apt-get install htop --y
 sudo apt-get install iptraf --y
